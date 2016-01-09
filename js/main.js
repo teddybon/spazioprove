8bbba63c4ac85453703dbc92435686b92db33bb0
Andrea Crisafulli <andrea.crisafulli@gmail.com>
2016-01-09 19:05:10 +0100

    Aggiunto struttura index.html + main.js +  per la gestione delle rotte + pagine home.html

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Main AngularJS
+ */
+var app = angular.module('spazioprova', [
+  'ngRoute'
+]);
+
+/**
+ * Configurazione ROTTE
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "pages/home.html", controller: "PageCtrl"})
+      // else 404
+    .otherwise("/404", {templateUrl: "pages/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controller per le Pagine del body
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Pages Controller log.");
